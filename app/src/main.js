9a33b51d274d6b7fd91eda4a72962c81f1d0cf62
Takashi Tamura <tamuratak@users.noreply.github.com>
2020-07-11 18:00:57 +0900

    edit

---
+++ app/src/main.js
@@ -0,0 +1,18 @@
+'use strict';
+const electron = require("electron");
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+let mainWindow;
+
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      webSecurity: false,
+      nodeIntegration: true
+    }
+  });
+  mainWindow.loadURL('file://' + __dirname + '/viewer/viewer.html');
+});
